f2d3746b877b13cb02cdeebb0864dfe2248bf683
Nuras1 <nurasbekmurad@mail.ru>
2026-06-08 21:00:27 +0500

    add streamlit7

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -356,27 +356,37 @@ st.plotly_chart(
 
 st.subheader("📈 Transaction Timeline")
 
+user_df["timestamp"] = pd.to_datetime(
+    user_df["timestamp"],
+    errors="coerce"
+)
+
 timeline = (
-
-    df.groupby(
-        df["timestamp"].dt.date
+    user_df
+    .dropna(subset=["timestamp"])
+    .groupby(
+        user_df["timestamp"].dt.date
     )
-
     .size()
-
     .reset_index(name="count")
 )
 
-fig_timeline = px.line(
-    timeline,
-    x="timestamp",
-    y="count"
-)
+if not timeline.empty:
 
-st.plotly_chart(
-    fig_timeline,
-    use_container_width=True
-)
+    fig_time = px.bar(
+        timeline,
+        x="timestamp",
+        y="count"
+    )
+
+    st.plotly_chart(
+        fig_time,
+        use_container_width=True
+    )
+
+else:
+
+    st.info("No timeline data available")
 
 # =====================================================
 # FRAUD BY COUNTRY
